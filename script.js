22f49c05f1ea778e0483855101e16af9e38967bb
lucasnather <natherlucas@gmail.com>
2025-02-27 22:39:32 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const container = document.querySelector('#container');
+
+container.addEventListener('click', () => {
+    alert("Você clicou no Serg.io - O melhor site do Brasil! - Lead BCT");
+})
